36f6b9be40810f3eb257fb8e69e16ad89c52aa91
yookdoyeon <yookdy2345@gmail.com>
2026-01-09 23:47:51 +0900

    Branch test

---
+++ branch_test/branch_test.swift
@@ -0,0 +1,18 @@
+//
+//  brench_test.swift
+//  github_test
+//
+//  Created by 육도연 on 1/9/26.
+//
+
+import SwiftUI
+
+struct brench_test: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview{
+    brench_test()
+}
